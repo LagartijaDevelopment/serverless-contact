812099ba67ea8400e1b7a7856f54da6e20ff5ec4
Guillermo Omar Lopez Lopez <omar@nutriconsultor.io>
2019-01-20 19:34:34 -0800

    feat: now use database to get recipients and valid origins.

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -8,14 +8,25 @@ import { template } from './mail-template';
 
 mail.setApiKey(functions.config().sendgrid.key);
 admin.initializeApp();
+const db = admin.firestore();
 
 const main = express();
 main.use(bodyParser.json());
 main.use(bodyParser.urlencoded({ extended: false }));
 
+main.use(async function (req, res, next) {
+    const origin = await req.header('Origin') || req.header('origin');
+    if (validateOrigin(origin)) {
+        res.header('Access-Control-Allow-Origin', origin);
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    }
+    next();
+});
+
 main.post('/:to', async (req, res) => {
+    const to = await db.collection('recipients').doc(req.params.to).get();
     const mailData: any = {
-        to: functions.config().emails[req.params.to],
+        to: to.data(),
         from: {
             email: req.body.email,
             name: req.body.name
@@ -43,4 +54,15 @@ main.post('/:to', async (req, res) => {
     });
 });
 
+async function validateOrigin(testOrigin) {
+    const sites = await db.collection('sites').get();
+    let valid: boolean = false;
+    sites.forEach(site => {
+        const data = site.data();
+        if (data.origin === testOrigin) {
+            valid = true;
+        }
+    });
+    return valid;
+}
 export const api = functions.https.onRequest(main);
